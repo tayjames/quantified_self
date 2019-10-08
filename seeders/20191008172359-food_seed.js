74fa96ecb27c788686c83f39f0cdbbacc16a7d1c
Tay <tay.james.d@gmail.com>
2019-10-08 11:46:32 -0600

    Creates food seeds

---
+++ seeders/20191008172359-food_seed.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Foods', [
+      {
+      "id": 1,
+      "name": "Banana",
+      "calories": 150,
+      "createdAt": new Date(),
+      "updatedAt": new Date()
+      },
+      {
+      "id": 2,
+      "name": "Apple",
+      "calories": 200,
+      "createdAt": new Date(),
+      "updatedAt": new Date()
+      }
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Foods', null, {});
+  }
+};
