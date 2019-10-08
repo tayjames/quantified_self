06185cac34ada4fe5aab35ff56134bfe6957b952
Tay <tay.james.d@gmail.com>
2019-10-08 11:46:14 -0600

    Pluralizes Foods table

--- migrations/20191008170648-create-food.js
+++ migrations/20191008170648-create-food.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Food', {
+    return queryInterface.createTable('Foods', {
       id: {
         allowNull: false,
         autoIncrement: true,
